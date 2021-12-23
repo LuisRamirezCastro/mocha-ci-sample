8b28b86b1f7d9bf32d6486c65898649720f2da7c
ralu1002 <luis.ramirez.consultant@nielseniq.com>
2021-12-23 10:08:48 -0600

    Fixing Node Test

--- src/version.js
+++ src/version.js
@@ -1,7 +1,7 @@
 module.exports =  getNodeVersion = function(firstName, lastName){
     const currentVersion = process.version;
     console.log(currentVersion);
-    if (currentVersion.includes('18')){
+    if (currentVersion.includes('17')){
         throw Error('Version is not supported')
     }
     return true;
